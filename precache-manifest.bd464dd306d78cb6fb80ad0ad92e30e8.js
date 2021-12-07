ca45bdedf1f296513e1af9ca6f171335e7e7dbe8
Szymon Skorupinski <sskorupinski@wp.pl>
2021-12-07 13:43:16 +0100

    Updates

---
+++ precache-manifest.bd464dd306d78cb6fb80ad0ad92e30e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1a82ce522e4ed34bfbace488dc3dd93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f56f601f69dd32e9b4b",
+    "url": "/static/css/main.931f6c58.chunk.css"
+  },
+  {
+    "revision": "8bd0541c4a70486f4fac",
+    "url": "/static/js/2.c7cf932e.chunk.js"
+  },
+  {
+    "revision": "6a29b30dd9e8ef5706396c026cb51452",
+    "url": "/static/js/2.c7cf932e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f56f601f69dd32e9b4b",
+    "url": "/static/js/main.f5e44e9f.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
